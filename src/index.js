1b12918f5059c0895c3e10d1c659d0a2a0adcc8a
Daniel Mituku <dberhe490@gmail.com>
2024-06-01 01:04:11 +0300

    file added

--- src/index.js
+++ src/index.js
@@ -1,40 +1,37 @@
 import React  from "react";
 import ReactDom from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spineer";
 
 
 class App extends React.Component {
     
-    constructor(props){
-        super(props);
-        this.state = { lat: null, errorMessage: ''};
+    state = {lat: null, errorMessage: ''}
 
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
-            (position) => this.setState({lat: position.coords.latitude}),
-             (err) => this.setState({errorMessage: err.message})
+            position => this.setState({lat: position.coords.latitude}),err => this.setState({errorMessage: err.message})
         );
     }
 
-    componentDidMount(){
-        console.log("My compnent just was rerenderd to the screen");
-    }
-
-    componentDidUpdate(){
-        console.log("My component just updated --> it rerendered!!");
-    }
-    
-    //React says we have to define render method
-    render(){
+    renderContent(){
         if(this.state.errorMessage && !this.state.lat){
             return <div>Error: {this.state.errorMessage}</div>
         }
 
         if(!this.state.errorMessage && this.state.lat){
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat}/>
         }
-        return <div>Loding!</div>;
-    }
-}
+        return <Spinner message= "Please allow the location request!"/>;
+    };
 
+    //React says we have to define render method
+    render(){
+        return(
+            <div className="border red">
+               {this.renderContent()}
+            </div>
+        )}
+    }
 
 ReactDom.render(<App />, document.querySelector('#root'));
